4bb6b6813327ec2e97159ab7448f20841222cfe8
JayanthKumarKollu <jayanthjai8464@gmail.com>
2026-06-14 22:45:49 +0530

    added code to convert all the letters to upper case

--- Backend/src/Controllers/user.js
+++ Backend/src/Controllers/user.js
@@ -1,20 +1,22 @@
 const userDetails = require("../Models/userDetails");
 
-const addUserDetails = async (req,res)=>{
-     try {
-         const user= await userDetails.findOne({'name':req.body.name});
-         if(!user){
-             await userDetails.insertOne(req.body);
-             res.status(200).json({msg:"User added Successfully."})
-         }
-         else{
-             res.status(400).json({msg:"User already exist."})
-         }
-        
-     } catch (error) {
-         res.status(500).json({mesg:error})
-     }
+const addUserDetails = async (req, res) => {
+  try {
+    Object.keys(req.body).forEach((key) => {
+      if (typeof req.body[key] === "string") {
+        req.body[key] = req.body[key].toUpperCase();
+      }
+    });
+    const user = await userDetails.findOne({ name: req.body.name });
+    if (!user) {
+      await userDetails.insertOne(req.body);
+      res.status(200).json({ msg: "User added Successfully." });
+    } else {
+      res.status(400).json({ msg: "User already exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ mesg: error });
+  }
+};
 
-}
-
-module.exports={addUserDetails};
+module.exports = { addUserDetails };
